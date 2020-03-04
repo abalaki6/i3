d0d209614aab296cb6afeefe23739d3e479c690a
Artsiom Balakir <artsiombalakir@gmail.com>
2020-03-03 22:33:11 -0800

    updated config

--- update_config.sh
+++ update_config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # copy a local config
-
-# if regolith
-cp ~/.config/regolith/i3/config .
-# if arch
-# cp ~/.config/i3/config .
+if [[ $(uname -r) == *"ARCH"* ]]; then
+  cp ~/.config/i3/config .
+# assume then regolith
+else
+  cp ~/.config/regolith/i3/config .
+fi
 
 git add config
 git commit -m "updated config"
